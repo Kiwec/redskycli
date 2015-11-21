11873e657eb2e9e1339bd4ec1a279a6527f85144
Wolf Clément <wolf.clement@yahoo.fr>
2015-11-21 21:43:23 +0100

    Added Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.address = 'http://skychat.fr:8054';
+config.id = 'id ***REMOVED***ici';
+config.ignoreList = [];
+config.hash = 'hash ***REMOVED***ici';
+config.username = 'pseudo par defaut ici';
+
+module.exports = config;
