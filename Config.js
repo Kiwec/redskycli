21a05400b76175c236f63d7c3c7f25ed2cb3ec8b
Wolf Clément <wolf.clement@yahoo.fr>
2015-11-21 21:43:23 +0100

    Added Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.address = 'http://redsky.fr:8054';
+config.id = 'id redsky ici';
+config.ignoreList = [];
+config.hash = 'hash redsky ici';
+config.username = 'pseudo par defaut ici';
+
+module.exports = config;
