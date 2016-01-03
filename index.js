738fe9266d0e7f65216721a538ee4668161e742a
Kiwec <c.wolf@kiwec.net>
2016-01-03 15:22:38 +0100

    Remake

--- index.js
+++ index.js
@@ -1,192 +1,25 @@
-var cheerio = require('cheerio');
-var io = require('socket.io-client');
-var readline = require('readline');
-var setcolor = require('ansi-color').set;
+var Config = require('./config');
 var fs = require('fs');
+var readline = require('readline');
+var SkyChat = require('./skychat-chat');
+var Util = require('./skychat-util');
 
-var Util = {
-  filter: function(msg) {
-    // Citation
-    var citation = msg.match(/<bl.*?>(.*?)<\/b.*?e>/);
-    if(citation) {
-      var user_cite = citation[0].match(/.*?>Par (.*?)<.*?r>/);
-      citation = citation[0].replace(/.*?r>/, '');
-      var message = msg.replace(/.*?ote>/, '');
-      msg = setcolor('[' + user_cite[1] + '] ' + citation, 'italic');
-      if(message !== '') msg += '\n  -&gt; ' + message;
-    }
-    // Smileys
-    var tags = msg.match(/<.*?>/g);
-    if(tags) {
-      for(var i = 0; i < tags.length; i++) {
-        var alt = tags[i].match(/alt="(.*?)"/);
-        if(alt && alt.length == 2) {
-          msg = msg.replace(tags[i], alt[1]);
-        } else {
-          msg = msg.replace(tags[i], '');
-        }
-      }
-    }
+SkyChat.on('newmessage', log);
+SkyChat.on('server_info', function(msg) {
+	if(msg.message !== 'undefined') log(msg);
+});
 
-    // Code html
-    var $ = cheerio.load('');
-    msg = $('<p>' + msg + '</p>').text();
-
-    return msg;
-  },
-  fixColor: function(color) {
-    if(typeof color === 'undefined' || color === false) color = 'white';
-		// Admin
-    else if(color == '???') color = setcolor('red', 'bold');
-		// Modérateur
-    else if(color == '#457dbb') color = 'red';
-		// Manager
-    else if(color == '#bf00ff') color = 'green';
-		// Alpha
-    else if(color == '???') color = 'blue';
-		// Beta
-    else if(color == '#85c630') color = 'cyan';
-		// VIP
-		else if(color == '???') color = 'yellow';
-		// Sandale
-		else if(color == '#f5a6bf') color = 'magenta';
-    else color = 'white';
-    return color;
-  },
-  showMessage: function(msg) { 
-    if(msg.message_type == 'bot_message')
-      chat.log('[BOT] ' + this.filter(msg.message), 'blue');
-    else if(msg.message_type == 'user_message')
-      chat.log('[' + setcolor(msg.pseudo, msg.color) + '] ' + this.filter(msg.message));
-    else if(msg.message_type == 'user_me')
-      chat.log(setcolor('* ' + setcolor(msg.pseudo, msg.color), 'italic') + ' ' +
-	setcolor(this.filter(msg.message) + ' *', 'italic'));
-    else if(msg.message_type == 'user_mp')
-      chat.log(setcolor('[MP]', 'green') + ' [' + setcolor(msg.pseudo, msg.color) +
-	'] ' + setcolor(this.filter(msg.message), 'green'));
-    else if(msg.message_type == 'user_spoil')
-      chat.log('[' + setcolor(msg.pseudo, msg.color) + ']' +
-	setcolor(setcolor(' [SPOIL] ', 'red'), 'bold') + this.filter(msg.message));
-    else chat.log('Message inconnu !', 'red');
-  }
-};
-
-function Chat()
-{
-  this.buffer = [];
-  this.connected = false;
-  this.config = require('./Config');
+function log(msg) {
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	Util.showMessage(msg);
+	fs.appendFile('log.txt', msg + '\n');
+	rl.prompt(true);
 }
 
-Chat.prototype.init = function()
-{
-  this.username = this.config.username;
-
-  this.sock = io.connect(this.config.address);
-  this.sock.on('alert', this.onAlert.bind(this));
-  this.sock.on('ban', this.onBan.bind(this));
-  this.sock.on('clear', this.onClear.bind(this));
-  this.sock.on('info', this.onInfo.bind(this));
-  this.sock.on('log', this.onLog.bind(this));
-  this.sock.on('message', this.onMessage.bind(this));
-  this.sock.on('success', this.onSuccess.bind(this));
-  this.sock.on('youtube_player', this.onYoutube.bind(this));
-
-  this.sock.on('connect', this.relog.bind(this));
-};
-
-Chat.prototype.changeMask = function() {
-  // 0-40, 54-56
-  // Donc 0-43 et si > 40, on ajoute 13
-  var randomMask = Math.floor(Math.random() * 56);
-  this.sock.emit('message',{message:'/shop mask buy ' + randomMask});
-};
-
-Chat.prototype.relog = function() {
-  this.sock.emit('log', {
-    hash: this.config.hash,
-    pseudo: this.config.username,
-    id: this.config.id,
-    mobile: false
-  });
-};
-
-Chat.prototype.log = function(msg, color)
-{
-  if(typeof color !== 'undefined' && color !== false) {
-    msg = setcolor(msg, color);
-  }
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  console.log(msg);
-  fs.appendFile('log.txt', msg + '\n', function(err){});
-  rl.prompt(true);
-};
-
-Chat.prototype.onAlert = function(msg) {
-  this.log(msg.message, 'red');
-};
-Chat.prototype.onBan = function(msg) {
-  this.log('Banni :(', 'red');
-  this.sock.disconnect();
-};
-Chat.prototype.onClear = function(msg) { this.log('= Chat clear =', 'blue'); };
-Chat.prototype.onInfo = function(msg) {
-  if(msg.message.indexOf('Vos donn') !== 0) this.log(msg.message, 'blue');
-};
-Chat.prototype.onLog = function(msg) {
-  this.username = msg.pseudo;
-  if(this.buffer.length > 0) {
-    this.sock.emit('message',{message:this.buffer[0]});
-    this.buffer.splice(0, 1);
-    return;
-  }
-
-  this.connected = true;
-  this.log('Connecte sous ' + msg.pseudo, 'green');
-};
-Chat.prototype.onMessage = function(msg) {
- 
-  if(Util.filter(msg.message).indexOf('!play ') === 0) {
-    msg.message = Util.filter(msg.message);
-    msg.message = msg.message.split(' ')[1];
-    chat.log(msg.message);
-    msg.message = msg.message.substring(msg.message.indexOf('=') + 1);
-    chat.log(msg.message);
-    chat.send('/yt play ' + msg.message);
-  }
-
-  if(msg.pseudo_lower == this.username) {
-    if(Util.filter(msg.message) === '') this.log('SANDALE !', 'red');
-    return;
-  }
-
-  for(var i in this.config.ignoreList) {
-    if(this.config.ignoreList[i].toLowerCase() == msg.pseudo_lower) return;
-  }
-
-  msg.color = Util.fixColor(msg.color);
-  Util.showMessage(msg);
-
-  if(Util.filter(msg.message) == ':morsay:') this.send(':japfuck:');
-};
-Chat.prototype.onSuccess = function(msg) { this.log(msg.message, 'green'); };
-Chat.prototype.onYoutube = function(msg) {
-  this.log('Youtube : ' + msg.code, 'yellow');
-};
-
-// Envoie un message
-Chat.prototype.send = function(msg) {
-  this.sock.emit('message',{message:msg});
-  setTimeout(this.changeMask.bind(this), 300);
-};
-
-var chat = new Chat();
-chat.init();
-
 var rl = readline.createInterface(process.stdin, process.stdout);
-rl.setPrompt('[' + setcolor(chat.username, 'cyan') + '] ');
+rl.setPrompt(Config.username + '> ');
 rl.on('line', function(line) {
-  chat.send(line);
+  SkyChat.send(line);
   rl.prompt(true);
 });
---
+++ redsky-chat.js
@@ -0,0 +1,116 @@
+var Config = require('./config');
+var io = require('socket.io-client');
+var Util = require('./skychat-util');
+
+function SkyChat() {
+	this.events = {};
+	this.loggedin = false;
+	this.sock = io.connect('http://skychat.fr:8054');
+
+	var eventList = ['alert', 'connected_list', 'info', 'success'];
+	for (var i in eventList) {
+		this.sock.on(eventList[i], (function(eventArgs) {
+			this.fireEvent(eventList[i], eventArgs);
+		}).bind(this));
+	}
+
+	this.sock.on('connect', this.onConnect.bind(this));
+	this.sock.on('message', this.onMessage.bind(this));
+	this.sock.on('log', this.onLog.bind(this));
+}
+
+// Calls registered event callbacks
+SkyChat.prototype.fireEvent = function(eventName, eventArgs) {
+	switch(eventName) {
+		case 'alert':
+		case 'info':
+		case 'success':
+			this.onServerInfo(eventArgs);
+			break;
+		default:
+	}
+
+	if(typeof this.events[eventName] !== 'undefined') {
+		for (var i in this.events[eventName]) {
+			this.events[eventName][i](eventArgs);
+		}
+	}
+};
+
+SkyChat.prototype.mp = function(user, msg) {
+	this.send('/w ' + user + ' ' + msg);
+};
+
+// Registers a new event
+SkyChat.prototype.on = function(event, callback) {
+	if(typeof this.events[event] === 'undefined') {
+		this.events[event] = [];
+	}
+	this.events[event].push(callback);
+};
+
+SkyChat.prototype.onConnect = function() {
+	this.sock.emit('log', {
+		hash: Config.hash,
+		pseudo: Config.username,
+		id: Config.id,
+		mobile: Config.mobile
+	});
+};
+
+SkyChat.prototype.onMessage = function(msg) {
+	var cleanMsg = Util.filter(msg);
+	if(msg.tms * 1000 < Date.now() - 2000) return;
+	if(msg.message.indexOf('!') === 0) {
+		this.fireEvent('command', {
+			user: msg.pseudo,
+			name: cleanMsg.substring(1).split(' ')[0],
+			args: cleanMsg.substring(cleanMsg.indexOf(' ') + 1),
+			nbArgs: cleanMsg.split(' ').length - 1
+		});
+	} else if(msg.pseudo == 'SkychatBot') {
+		// Points transfer
+		if(cleanMsg.indexOf('de commission') !== -1) {
+			var giveMsg = msg.message.match(/<b>(.*?)<\/b>/g);
+			this.fireEvent('givepoints', {
+				from: Util.removeHTML(giveMsg[0]),
+				amount: parseInt(Util.removeHTML(giveMsg[1]), 10),
+				to: Util.removeHTML(giveMsg[2])
+			});
+		// Random number generation
+		} else if(cleanMsg.indexOf('tiré par') !== -1) {
+			var match = cleanMsg.match(/t (.*) t.*?r (.*): (.*)/);
+			this.fireEvent('rand', {
+				max: parseInt(match[1], 10),
+				pseudo: match[2],
+				number: parseInt(match[3], 10)
+			});
+		}
+	} else {
+	  this.fireEvent('newmessage', msg);
+	}
+};
+
+SkyChat.prototype.onLog = function(args) {
+	if(this.loggedin) return;
+	this.loggedin = true;
+	this.fireEvent('log_once', args);
+};
+
+SkyChat.prototype.onServerInfo = function(args) {
+	this.fireEvent('server_info', args);
+};
+
+SkyChat.prototype.send = function(message) {
+	this.sock.emit('message', { message: message });	
+};
+
+SkyChat.prototype.sendLater = function(msg, delay) {
+		// 400ms = safe delay between messages
+	if(typeof delay === 'undefined') delay = 400;
+	setTimeout((function() { this.send(msg); }).bind(this), delay);
+
+};
+
+module.exports = new SkyChat();
+
