2673215b26513ee41809b3347205e2431206f7f9
Kiwec <c.wolf@kiwec.net>
2016-01-21 18:13:34 +0100

    Rough update to module

--- Config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.address = 'http://skychat.fr:8054';
-config.id = 'id ***REMOVED***ici';
-config.ignoreList = [];
-config.hash = 'hash ***REMOVED***ici';
-config.username = 'pseudo par defaut ici';
-
-module.exports = config;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Config = require('./config');
 var fs = require('fs');
 var readline = require('readline');
-var SkyChat = require('./skychat-chat');
-var Util = require('./skychat-util');
+var SkyChatO = require('node-skychat');
+var SkyChat = new SkyChatO(Config);
 
 SkyChat.on('newmessage', log);
 SkyChat.on('server_info', function(msg) {
@@ -12,7 +12,8 @@ SkyChat.on('server_info', function(msg) {
 function log(msg) {
 	process.stdout.clearLine();
 	process.stdout.cursorTo(0);
-	Util.showMessage(msg);
+	msg = SkyChat.format(msg);
+	console.log(msg);
 	fs.appendFile('log.txt', msg + '\n');
 	rl.prompt(true);
 }
@@ -20,6 +21,8 @@ function log(msg) {
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt(Config.username + '> ');
 rl.on('line', function(line) {
+	var rng = Math.round(Math.random() * 63);
+	SkyChat.send('/shop mask buy ' + rng);
   SkyChat.send(line);
   rl.prompt(true);
 });
